14225736ca216cf12ecca169472e9b7129ab1e64
Samuel W <samueljwerner@gmail.com>
2016-01-22 15:11:09 -0600

    profile adjustments

--- resources/views/pages/public/profile.blade.php
+++ resources/views/pages/public/profile.blade.php
@@ -9,10 +9,10 @@
 		<h1>{{$user->full_name()}}<a href="/{{$user->slug}}/give" class="give-button button button-primary button-raised tiny">Give Now </a></h1>
 	@endif
 
-<div class="large-6 columns">
+<div class="large-6 medium-6 columns">
 <img class="public-profile-image" src="{{$user->image}}">
 </div>
-<div class="large-6 columns">
+<div class="large-6 medium-6 columns">
   <a target="_blank" href="{{$user->website}}">{{$user->website}}</a>
 </div>
 <p>
