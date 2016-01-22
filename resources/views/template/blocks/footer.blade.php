ddba4db0ce3186105105f8f25a6303f482832856
Samuel W <samueljwerner@gmail.com>
2016-01-22 15:43:05 -0600

    footer reworking

--- resources/views/template/blocks/footer.blade.php
+++ resources/views/template/blocks/footer.blade.php
@@ -1,7 +1,27 @@
 <div class="row">
-  <ul class="menu vertical">
-    <li><a href="/login">Login</a></li>
-    <li><a href="/legal">Legal Information</a></li>
-    <li><a href="/apply">Apply</a></li>
-  </ul>
+  <div class="large-4 columns">
+    <ul class="menu vertical">
+      <li><a href="/login">Login</a></li>
+      <li><a href="/legal">Legal Information</a></li>
+      <li><a href="/apply">Apply</a></li>
+    </ul>
+  </div>
+  <div class="large-4 columns text-center">
+      <img width="250" src="https://s3-us-west-2.amazonaws.com/acacia-ministries/images/invert-logo.png" alt="" />
+  </div>
+  <div class="large-4 columns text-right footer-more-info">
+
+    <p>
+      Get more information about our ministry
+    </p>
+    <form action="http://ma26.createsend.com/t/i/s/dulhjl/" method="post" id="subForm">
+      <div class="large-10 columns">
+        <input placeholder="Enter Your Email Address" id="fieldEmail" name="cm-dulhjl-dulhjl" type="email" required />
+      </div>
+      <div class="large-2 columns">
+        <button type="submit" class="button button-raised">Subscribe</button>
+      </div>
+</form>
+  </div>
+
 </div>
