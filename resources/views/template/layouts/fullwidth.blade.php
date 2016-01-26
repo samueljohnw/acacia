d61b5f28049cc03a03294c0860978bed6ad87643
Samuel W <samueljwerner@gmail.com>
2016-01-26 14:40:00 -0600

    asyc js

--- resources/views/template/layouts/fullwidth.blade.php
+++ resources/views/template/layouts/fullwidth.blade.php
@@ -20,7 +20,7 @@
     </footer>
 
 
-    <script src="{{ elixir('js/all.js') }}"></script>
+    <script async src="{{ elixir('js/all.js') }}"></script>
     <script>
       $(document).foundation();
     </script>
