085d6ca5cff2db213a8c626dd610795ff9ea5b90
Samuel W <samueljwerner@gmail.com>
2016-04-03 18:26:47 -0500

    cancel monthlies

--- resources/views/admin/monthlies/show.blade.php
+++ resources/views/admin/monthlies/show.blade.php
@@ -19,14 +19,45 @@ If the charge day has past, this will only cancel donations afterwards. If the c
 	<tbody>
 	@foreach($monthlies as $monthly)
 
-		<tr>
+		<tr class="tr-customer-{{$monthly->customer_id}}">
 			<td>{{$monthly->first_name}} {{$monthly->last_name}}</td>
 	        <td><a href="{{route('admin.users.edit', $monthly->user()->first()->id)}}"> {{$monthly->user()->first()->first_name}} {{$monthly->user()->first()->last_name}}</a></td>
 			<td>{{$monthly->amount}}</td>
-			<td>{{$monthly->created_at->format('dS')}}</td>	
+			<td>{{$monthly->created_at->format('dS')}}</td>
+			<td><button data-open="cancelModal{{$monthly->id}}" class="button button-raised button-action" >Cancel</button>
+				<div class="reveal" id="cancelModal{{$monthly->id}}" data-reveal>
+					<p>
+						Are you sure you want to cancel this monthly donation?
+					</p>
+					<a href="" class="js-cancel-monthly button" data-customer-id="{{$monthly->customer_id}}">Cancel</a>
+				</div>
+			</td>
 		</tr>
 
 	@endforeach
 	</tbody>
 </table>
+
 @endsection
+@section('footer-scripts')
+<script type="text/javascript">
+	$('.js-cancel-monthly').click(function(e){
+		e.preventDefault();
+
+		var customer_id = $(this).data('customer-id');
+
+		$('.reveal').foundation('close');
+		$('.tr-customer-'+customer_id).fadeOut();
+
+		$.post("{{route('admin.monthlies.delete')}}",
+			{
+				_token: "{{csrf_token()}}",
+				customer_id: customer_id
+			})
+			.done(function( data ) {
+				$('#cancelModal').foundation('close');
+			}
+		);
+	})
+</script>
+@stop
